a989bafa9958f7390494e35bd40fb4b158115026
brennx0r <brennx0r@gmail.com>
2016-05-25 12:37:11 -0700

    Adding whiteboarding programs
    These are questions that I have been asked in interviews.

---
+++ whiteboarding/print-1thru10-fivetimes.rb
@@ -0,0 +1,7 @@
+# Whiteboard question: Print numbers 1 through 10 five times.
+
+5.times {
+	(1..10).each {|i|
+		puts i
+	}
+}
---
+++ whiteboarding/prints-fib.rb
@@ -0,0 +1,23 @@
+# Calculate numbers in the Fibonacci sequence
+# Example: 0,1,1,2,3,5,8,13
+
+
+def fib(n)
+	if n <= 1
+		puts 0 
+		return 0
+	end
+	if n == 2
+		puts n-1
+		return n-1
+	end
+	if n >= 3 
+		i = fib(n-1).to_i+fib(n-2).to_i
+		puts i
+		return i
+	end
+end
+
+puts "What fibonacci series number would you like to retrieve?"
+n = gets.chomp.to_i
+fib(n)
