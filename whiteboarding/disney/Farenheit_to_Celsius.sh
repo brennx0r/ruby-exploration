c4e8bc07e2b9e955a6e41e73b1c5ddea8c1546f2
brennx0r <brennx0r@gmail.com>
2016-06-17 09:42:41 -0700

    Clean up comments, echo statement
    The comments and echo message needed clarification on numbers accepted for the program.

--- whiteboarding/disney/Farenheit_to_Celsius.sh
+++ whiteboarding/disney/Farenheit_to_Celsius.sh
@@ -5,14 +5,15 @@
 
 F=$1
 
-# We need to assert that the input is an integer.
-# If the number is not an integer, we exit and let the user know that a number is required.
-# Because the program will not complete successfully without an integer,
+# We need to assert that the input is a number.
+# If not a number, we exit and let the user know that a number is required.
+# Because the program will not complete successfully without an number,
 # we need to exit the program as non zero.
 
 number_regex='-?[0-9]+([.][0-9]+)?$'
+
 if ! [[ $F =~ $number_regex ]] ; then
-   echo "Error - Please use a whole positive or negative number for input." >&2; exit 1
+   echo "Error - Please use a number for input." >&2; exit 1
 fi
 
 C=($F-32)/1.8
