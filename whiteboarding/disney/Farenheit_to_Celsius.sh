3b921354177b936eed0585f8f48fe8fd122b5186
brennx0r <brennx0r@gmail.com>
2016-06-17 09:24:34 -0700

    Add logic to ensure input is a whole number
    Adding logic in the program that will check to ensure that the input used in the program is a number.
    
    For now, we restrict the program to accept whole numbers (later revisions will be able to handle floats).
    
    Because we need a number in order to have the program run to successful completion, we will exit code 1 if the input is not a number.

--- whiteboarding/disney/Farenheit_to_Celsius.sh
+++ whiteboarding/disney/Farenheit_to_Celsius.sh
@@ -4,5 +4,17 @@
 # to Celsius.
 
 F=$1
+
+# We need to assert that the input is an integer.
+# If the number is not an integer, we exit and let the user know that a number is required.
+# Because the program will not complete successfully without an integer,
+# we need to exit the program as non zero.
+
+integer_regex='^[0-9]+$'
+if ! [[ $F =~ $integer_regex ]] ; then
+   echo "Error - Please use a whole positive or negative number for input." >&2; exit 1
+fi
+
+
 C=($F-32)/1.8
 echo "$C" | bc
